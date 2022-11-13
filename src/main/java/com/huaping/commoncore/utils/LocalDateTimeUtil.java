08484250687c907858f3430eef400a34fa0c5a06
huaping <huaping@example.com>
2022-11-13 15:26:52 +0800

    [Description]优化LocalDateTime工具类

--- src/main/java/com/huaping/commoncore/utils/LocalDateTimeUtil.java
+++ src/main/java/com/huaping/commoncore/utils/LocalDateTimeUtil.java
@@ -1,123 +1,185 @@
 package com.huaping.commoncore.utils;
 
+import org.springframework.util.StringUtils;
+
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 public class LocalDateTimeUtil {
-    private static final String DATE_FORMAT_6 = "yyyyMM";
-    private static final String DATE_FORMAT_8 = "yyyyMMdd";
-    private static final String DATE_FORMAT_10 = "yyyy-MM-dd";
-    private static final String DATE_FORMAT_16 = "yyyy-MM-dd HH:mm";
-    private static final String DATE_FORMAT_17 = "yyyyMMdd HH:mm:ss";
-    private static final String DATE_FORMAT_19 = "yyyy-MM-dd HH:mm:ss";
-    private static final String DATE_FORMAT_19_SLASH = "yyyy/MM/dd HH:mm:ss";
+    private static final String DATE6 = "yyyyMM";
+    private static final String DATE8 = "yyyyMMdd";
+    private static final String DATE10 = "yyyy-MM-dd";
+    private static final String DATE_TIME_16 = "yyyy-MM-dd HH:mm";
+    private static final String DATE_TIME_17 = "yyyyMMdd HH:mm:ss";
+    private static final String DATE_TIME_19 = "yyyy-MM-dd HH:mm:ss";
+    private static final String DATE_TIME_19_SLASH = "yyyy/MM/dd HH:mm:ss";
+    private static final String DATE_TIME_24 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
     private LocalDateTimeUtil() {
 
     }
 
-    public static String formatDate() {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT_19));
+    /**
+     * 返回当前时间的yyyy-MM-dd HH:mm:ss格式的字符串
+     *
+     * @return String  yyyy-MM-dd HH:mm:ss格式的字符串
+     */
+    public static String formatDateTime() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_TIME_19));
     }
 
-
-
     /**
-     * 返回yyyy-MM-dd HH:mm:ss格式的時間的字符串
+     * 返回yyyyMM格式的字符串
      *
-     * @param localDateTime
-     * @return
+     * @param localDateTime LocalDateTime对象
+     * @return yyyyMM格式的字符串
      */
     public static String formatDate6(LocalDateTime localDateTime) {
         if (localDateTime == null) {
             return null;
         }
 
-        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_FORMAT_6));
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE6));
     }
 
     /**
-     * 返回yyyy-MM-dd HH:mm:ss格式的時間的字符串
+     * 返回yyyyMMdd格式的字符串
      *
-     * @param localDateTime
-     * @return
+     * @param localDateTime LocalDateTime对象
+     * @return yyyyMMdd格式的字符串
      */
     public static String formatDate8(LocalDateTime localDateTime) {
         if (localDateTime == null) {
             return null;
         }
 
-        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_FORMAT_8));
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE8));
     }
 
     /**
-     * 返回yyyy-MM-dd HH:mm:ss格式的時間的字符串
+     * 返回yyyy-MM-dd格式的字符串
      *
-     * @param localDateTime
-     * @return
+     * @param localDateTime LocalDateTime对象
+     * @return yyyy-MM-dd格式的字符串
      */
     public static String formatDate10(LocalDateTime localDateTime) {
         if (localDateTime == null) {
             return null;
         }
 
-        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_FORMAT_10));
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE10));
     }
 
     /**
-     * 返回yyyy-MM-dd HH:mm:ss格式的時間的字符串
+     * 返回yyyy-MM-dd HH:mm格式的字符串
      *
-     * @param localDateTime
-     * @return
+     * @param localDateTime LocalDateTime对象
+     * @return yyyy-MM-dd HH:mm格式的字符串
      */
-    public static String formatDate16(LocalDateTime localDateTime) {
+    public static String formatDateTime16(LocalDateTime localDateTime) {
         if (localDateTime == null) {
             return null;
         }
 
-        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_FORMAT_16));
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_16));
     }
 
     /**
-     * 返回yyyy-MM-dd HH:mm:ss格式的時間的字符串
+     * 返回yyyyMMdd HH:mm:ss格式的字符串
      *
-     * @param localDateTime
-     * @return
+     * @param localDateTime LocalDateTime对象
+     * @return yyyyMMdd HH:mm:ss格式的字符串
      */
-    public static String formatDate17(LocalDateTime localDateTime) {
+    public static String formatDateTime17(LocalDateTime localDateTime) {
         if (localDateTime == null) {
             return null;
         }
 
-        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_FORMAT_17));
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_17));
     }
 
     /**
-     * 返回yyyy-MM-dd HH:mm:ss格式的時間的字符串
+     * 返回yyyy-MM-dd HH:mm:ss格式的字符串
      *
-     * @param localDateTime
-     * @return
+     * @param localDateTime LocalDateTime对象
+     * @return yyyy-MM-dd HH:mm:ss格式的字符串
      */
-    public static String formatDate19(LocalDateTime localDateTime) {
+    public static String formatDateTime19(LocalDateTime localDateTime) {
         if (localDateTime == null) {
             return null;
         }
 
-        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_FORMAT_19));
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_19));
     }
 
     /**
-     * 返回yyyy-MM-dd HH:mm:ss格式的時間的字符串
+     * 返回yyyy/MM/dd HH:mm:ss格式的字符串
      *
-     * @param localDateTime
-     * @return
+     * @param localDateTime LocalDateTime对象
+     * @return yyyy/MM/dd HH:mm:ss格式的字符串
      */
-    public static String formatDate19Slash(LocalDateTime localDateTime) {
+    public static String formatDateTime19Slash(LocalDateTime localDateTime) {
         if (localDateTime == null) {
             return null;
         }
 
-        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_FORMAT_19_SLASH));
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_19_SLASH));
     }
 
+    /**
+     * 返回yyyy-MM-dd'T'HH:mm:ss.SSSZ格式的字符串
+     *
+     * @param localDateTime LocalDateTime对象
+     * @return yyyy-MM-dd'T'HH:mm:ss.SSSZ格式的字符串
+     */
+    public static String formatDateTime24(LocalDateTime localDateTime) {
+        if (localDateTime == null) {
+            return null;
+        }
+
+        return ZonedDateTime.of(localDateTime, ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern(DATE_TIME_24));
+    }
+
+    /**
+     * 返回自定义日期格式的字符串
+     *
+     * @param localDateTime LocalDateTime对象
+     * @param format 需要转换的格式
+     * @return 返回自定义日期格式的字符串
+     */
+    public static String formatDateTime(LocalDateTime localDateTime, String format) {
+        if (localDateTime == null) {
+            return null;
+        }
+
+        return localDateTime.format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static LocalDateTime parseDate8(String dateStr) {
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .appendPattern("yyyyMMdd")
+                .toFormatter();
+        return LocalDateTime.parse(dateStr, formatter);
+    }
+
+    public static LocalDateTime parseDate10(String dateStr) {
+
+        return LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern(DATE10));
+    }
+
+    public static LocalDateTime parseDateTime19(String dateTimeStr) {
+
+        return LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern(DATE_TIME_19));
+    }
+
+    public static LocalDateTime convertDateTime24to19(LocalDateTime localDateTime) {
+        if (localDateTime == null) {
+            return null;
+        }
+        return parseDateTime19(formatDateTime19(localDateTime));
+    }
 }
--- src/test/java/com/huaping/commoncore/utils/LocalDateTimeUtilTest.java
+++ src/test/java/com/huaping/commoncore/utils/LocalDateTimeUtilTest.java
@@ -1,14 +1,68 @@
 package com.huaping.commoncore.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.junit.jupiter.api.Test;
 
-import javax.annotation.Resource;
 import java.time.LocalDateTime;
 
+@Slf4j
 public class LocalDateTimeUtilTest {
+    /**
+     * testFormatDate6
+     */
     @Test
-    public void testLocalDateTimeUtil() {
-        System.out.println(LocalDateTimeUtil.formatDate19Slash(LocalDateTime.now()));
+    public void testFormatDate6() {
+        LocalDateTime localDateTime = LocalDateTime.of(2022, 11, 6, 23, 12, 32);
+        String dateStr = LocalDateTimeUtil.formatDate6(localDateTime);
+        log.info("dateStr={}", dateStr);
     }
 
+    /**
+     * testFormatDate8
+     */
+    @Test
+    public void testFormatDate8() {
+        String dateStr = LocalDateTimeUtil.formatDate8(LocalDateTime.now());
+        log.info("dateStr={}", dateStr);
+    }
+
+    /**
+     * testFormatDate10
+     */
+    @Test
+    public void testFormatDate10() {
+        String dateStr = LocalDateTimeUtil.formatDate10(LocalDateTime.now().plusDays(1));
+        log.info("dateStr={}", dateStr);
+    }
+
+    /**
+     * testFormatDateTime17
+     */
+    @Test
+    public void testFormatDateTime17() {
+        LocalDateTime localDateTime = LocalDateTime.of(2022, 11, 3, 17, 23, 32, 23);
+        String dateStr = LocalDateTimeUtil.formatDateTime17(localDateTime);
+        log.info("dateStr:{}", dateStr);
+    }
+
+    /**
+     * testParseDate19
+     */
+    @Test
+    public void testParseDate19() {
+        String dateStr = LocalDateTimeUtil.formatDateTime19(LocalDateTime.now());
+        log.info("dateStr={}", dateStr);
+        LocalDateTime localDateTime = LocalDateTimeUtil.parseDateTime19(dateStr);
+        log.info("localDateTime={}", localDateTime);
+    }
+
+    /**
+     * testConvertDateTime24to19
+     */
+    @Test
+    public void testConvertDateTime24to19() {
+        LocalDateTime localDateTime = LocalDateTimeUtil.convertDateTime24to19(LocalDateTime.now().minusMinutes(2));
+        log.info("localDateTime={}", localDateTime);
+    }
 }
